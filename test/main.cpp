0f5dc4225a61ce3f7a054731471a645ea94daa17
chunjuLin <linchun0308@gmail.com>
2021-04-07 02:11:54 -0700

    add FFT.py

--- test/main.cpp
+++ test/main.cpp
@@ -10,7 +10,12 @@ DigitalIn selected(D5);
 AnalogOut aout(PA_4);
 AnalogIn ain(D6);
 
+EventQueue queue(32 * EVENTS_EVENT_SIZE);
+
+
 Thread thread;
+
+
 float frequency = 4.166; 
 int slew = 1; 
 float blank = 100;
@@ -21,18 +26,207 @@ float sample;
 float ADCdata[5000];
 int k = 0;
 
-void get_point(){
-   // while(1){
-       /* for (int i = 0; i < 1000; i++){
-            sample = ain;
-            ADCdata[i] = sample;
-            //ThisThread::sleep_for(1);
-            wait_us(1000);
-        }*/
-        for (int i = 0; i < 1000; i++){
-            printf("%f\r\n", ADCdata[i]);
+
+
+void wave(){
+    float T = 0.24;
+    float dt = 1000;
+if(slew == 1){
+       k = 0;
+       while(k < 1000){
+        for (float i = 0.00f; i <= 0.9f; i += 0.09f) {
+                aout = i;          
+                wait_us((int)dt); k++;
+            }
+            aout = 0.9f; wait_us((int)dt*220);
+            for (float i = 0.9f; i >= 0.0f; i -= 0.09f) {
+                aout = i;
+                wait_us((int)dt); k++;
+                //printf("Hi4\n");
+            }
+       }
+       k = 0;
+       while(k < 1000){
+        for (float i = 0.00f; i <= 0.9f; i += 0.09f) {
+                aout = i; 
+                ADCdata[k] = aout; k++;        
+                wait_us((int)dt); 
+            }
+        for (float i = 0; i < 220; i++){
+            aout = 0.9f; 
+            ADCdata[k] = aout; k++;
+            wait_us((int)dt);
         }
-   // }
+            for (float i = 0.9f; i >= 0.0f; i -= 0.09f) {
+                aout = i;
+                ADCdata[k] = aout; k++;
+                wait_us((int)dt); 
+                //printf("Hi4\n");
+            }
+       }
+    for (int i = 0; i < 1000; i++){
+        printf("%f\r\n", ADCdata[i]);
+    }
+        while(1){
+        for (float i = 0.00f; i <= 0.9f; i += 0.09f) {
+                aout = i;          
+                wait_us((int)dt); k++;
+            }
+            aout = 0.9f; wait_us((int)dt*220);
+            for (float i = 0.9f; i >= 0.0f; i -= 0.09f) {
+                aout = i;
+                wait_us((int)dt); k++;
+                //printf("Hi4\n");
+            }
+       }
+   }
+    else if(slew == 2){
+       k = 0;
+       while(k < 1000){
+        for (float i = 0.00f; i <= 0.9f; i += 0.045f) {
+                aout = i;          
+                wait_us((int)dt); k++;
+            }
+            aout = 0.9f; wait_us((int)dt*200);
+            for (float i = 0.9f; i >= 0.0f; i -= 0.045f) {
+                aout = i;
+                wait_us((int)dt); k++;
+                //printf("Hi4\n");
+            }
+       }
+       k = 0;
+       while(k < 1000){
+        for (float i = 0.00f; i <= 0.9f; i += 0.045f) {
+                aout = i; 
+                ADCdata[k] = aout; k++;        
+                wait_us((int)dt); 
+            }
+        for (float i = 0; i < 200; i++){
+            aout = 0.9f; 
+            ADCdata[k] = aout; k++;
+            wait_us((int)dt);
+        }
+            for (float i = 0.9f; i >= 0.0f; i -= 0.045f) {
+                aout = i;
+                ADCdata[k] = aout; k++;
+                wait_us((int)dt); 
+                //printf("Hi4\n");
+            }
+       }
+    for (int i = 0; i < 1000; i++){
+        printf("%f\r\n", ADCdata[i]);
+    }
+        while(1){
+        for (float i = 0.00f; i <= 0.9f; i += 0.045f) {
+                aout = i;          
+                wait_us((int)dt); k++;
+            }
+            aout = 0.9f; wait_us((int)dt*200);
+            for (float i = 0.9f; i >= 0.0f; i -= 0.045f) {
+                aout = i;
+                wait_us((int)dt); k++;
+                //printf("Hi4\n");
+            }
+       }
+   }
+    else if(slew == 3){
+       k = 0;
+       while(k < 1000){
+        for (float i = 0.00f; i <= 0.9f; i += 0.0225f) {
+                aout = i;          
+                wait_us((int)dt); k++;
+            }
+            aout = 0.9f; wait_us((int)dt*160);
+            for (float i = 0.9f; i >= 0.0f; i -= 0.0225f) {
+                aout = i;
+                wait_us((int)dt); k++;
+                //printf("Hi4\n");
+            }
+       }
+       k = 0;
+       while(k < 1000){
+        for (float i = 0.00f; i <= 0.9f; i += 0.0225f) {
+                aout = i; 
+                ADCdata[k] = aout; k++;        
+                wait_us((int)dt); 
+            }
+        for (float i = 0; i < 160; i++){
+            aout = 0.9f; 
+            ADCdata[k] = aout; k++;
+            wait_us((int)dt);
+        }
+            for (float i = 0.9f; i >= 0.0f; i -= 0.0225f) {
+                aout = i;
+                ADCdata[k] = aout; k++;
+                wait_us((int)dt); 
+                //printf("Hi4\n");
+            }
+       }
+    for (int i = 0; i < 1000; i++){
+        printf("%f\r\n", ADCdata[i]);
+    }
+        while(1){
+        for (float i = 0.00f; i <= 0.9f; i += 0.0225f) {
+                aout = i;          
+                wait_us((int)dt); k++;
+            }
+            aout = 0.9f; wait_us((int)dt*160);
+            for (float i = 0.9f; i >= 0.0f; i -= 0.0225f) {
+                aout = i;
+                wait_us((int)dt); k++;
+                //printf("Hi4\n");
+            }
+       }
+   }
+    else if(slew == 4){
+       k = 0;
+       while(k < 1000){
+        for (float i = 0.00f; i <= 0.9f; i += 0.01125f) {
+                aout = i;          
+                wait_us((int)dt); k++;
+            }
+            aout = 0.9f; wait_us((int)dt*80);
+            for (float i = 0.9f; i >= 0.0f; i -= 0.01125f) {
+                aout = i;
+                wait_us((int)dt); k++;
+                //printf("Hi4\n");
+            }
+       }
+       k = 0;
+       while(k < 1000){
+        for (float i = 0.00f; i <= 0.9f; i += 0.01125f) {
+                aout = i; 
+                ADCdata[k] = aout; k++;        
+                wait_us((int)dt); 
+            }
+        for (float i = 0; i < 80; i++){
+            aout = 0.9f; 
+            ADCdata[k] = aout; k++;
+            wait_us((int)dt);
+        }
+            for (float i = 0.9f; i >= 0.0f; i -= 0.01125f) {
+                aout = i;
+                ADCdata[k] = aout; k++;
+                wait_us((int)dt); 
+                //printf("Hi4\n");
+            }
+       }
+    for (int i = 0; i < 1000; i++){
+        printf("%f\r\n", ADCdata[i]);
+    }
+        while(1){
+        for (float i = 0.00f; i <= 0.9f; i += 0.01125f) {
+                aout = i;          
+                wait_us((int)dt); k++;
+            }
+            aout = 0.9f; wait_us((int)dt*80);
+            for (float i = 0.9f; i >= 0.0f; i -= 0.01125f) {
+                aout = i;
+                wait_us((int)dt); k++;
+                //printf("Hi4\n");
+            }
+       }
+    }
 }
 
 int main()
@@ -74,122 +268,10 @@ int main()
         // if(frequency != 130)uLCD.filled_rectangle(30 + (blank)/10, 56, 98, 72, BLACK);
         // uLCD.line(99, 55, 99, 73, WHITE);
    }
-    float T = 0.24;
-    float dt = 1000;
-    printf("%d\n",4.166*100);
-   // thread.start(get_point);
-   if(slew == 1){
-       k = 0;
-       while(k < 1000){
-        for (float i = 0.00f; i <= 0.9f; i += 0.09f) {
-                aout = i;          
-                wait_us((int)dt); k++;
-            }
-            aout = 0.9f; wait_us((int)dt*220);
-            for (float i = 0.9f; i >= 0.0f; i -= 0.09f) {
-                aout = i;
-                wait_us((int)dt); k++;
-                //printf("Hi4\n");
-            }
-       }
-       k = 0;
-       while(k < 1000){
-        for (float i = 0.00f; i <= 0.9f; i += 0.09f) {
-                aout = i; 
-                         
-                wait_us((int)dt); k++;
-            }
-            aout = 0.9f; wait_us((int)dt*220);
-            for (float i = 0.9f; i >= 0.0f; i -= 0.09f) {
-                aout = i;
-                wait_us((int)dt); k++;
-                //printf("Hi4\n");
-            }
-       }
-   }
-    else if(slew == 2){
-    while(1){
-       for (float i = 0.00f; i <= 0.9f; i += 0.045f) {
-            aout = i;          
-            wait_us((int)dt); //k++;
-        }
-        aout = 0.9f; wait_us((int)dt*200);
-        for (float i = 0.9f; i >= 0.0f; i -= 0.045f) {
-            aout = i;
-            wait_us((int)dt); //k++;
-            //printf("Hi4\n");
-        }
-    }
-   }
-    else if(slew == 3){
-    while(1){
-       for (float i = 0.00f; i <= 0.9f; i += 0.0225f) {
-            aout = i;          
-            wait_us((int)dt); //k++;
-        }
-        aout = 0.9f; wait_us((int)dt*160);
-        for (float i = 0.9f; i >= 0.0f; i -= 0.0225f) {
-            aout = i;
-            wait_us((int)dt); //k++;
-            //printf("Hi4\n");
-        }
-    }
-   }
-    else if(slew == 4){
-    while(1){
-       for (float i = 0.00f; i <= 0.9f; i += 0.01125f) {
-            aout = i;          
-            wait_us((int)dt); //k++;
-        }
-        aout = 0.9f; wait_us((int)dt*80);
-        for (float i = 0.9f; i >= 0.0f; i -= 0.01125f) {
-            aout = i;
-            wait_us((int)dt); //k++;
-            //printf("Hi4\n");
-        }
-    }
-   }
-    //     float T = 0.24;
-    //     float dt = T / 100 * 1000000;
-    //     printf("%d\n",frequency*100);
-    //     while (k < 1000){
-    //         for (float i = 0.1125f; i <= 0.9f; i += 0.009844f) {
-    //             aout = i;          
-    //             wait_us((int)dt); k++;
-    //         }
-    //         for (float i = 0.9f; i >= 0.0f; i -= 0.045f) {
-    //             aout = i;
-    //             wait_us((int)dt); k++;
-    //             //printf("Hi4\n");
-    //         }
-    //     }
-    //     k = 0;
-    //    // printf("Hi\n");
-    //     while (k < 1000){
-    //         for (float i = 0.1125f; i <= 0.9f; i += 0.009844f) {
-    //             aout = i;
-    //             ADCdata[k] = ain; k++;           
-    //             wait_us((int)dt);
-    //             //printf("Hi1\n");
-    //         }
-    //         for (float i = 0.9f; i >= 0.0f; i -= 0.045f) {
-    //             aout = i;
-    //             ADCdata[k] = ain; k++;
-    //             wait_us((int)dt);
-    //             //printf("H2\n");
-    //         }
-    //     }
-    //     for (int i = 0; i < 1000; i++){
-    //         printf("%f\r\n", ADCdata[i]);
-    //     }
-    //     while (done == 1){
-    //         for (float i = 0.1125f; i <= 0.9f; i += 0.009844f) {
-    //             aout = i;           
-    //             wait_us((int)dt);
-    //         }
-    //         for (float i = 0.9f; i >= 0.0f; i -= 0.045f) {
-    //             aout = i;
-    //             wait_us((int)dt);
-    //         }
-    //     }
+
+    //printf("%d\n",4.166*240);
+    thread.start(callback(&queue, &EventQueue::dispatch_forever));
+    queue.call(wave);
+   
+
 }
